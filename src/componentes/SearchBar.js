4bc3ef341dc31885370ff5f167737be9308be697
Reginaldo Santos <rmsantos@br.ibm.com>
2018-10-11 13:49:18 -0300

    função Pesquisa Termo que captura o que o usuário digitou no input

--- src/componentes/SearchBar.js
+++ src/componentes/SearchBar.js
@@ -2,11 +2,19 @@ import React, {Component} from 'react'
 import {Segment, Input} from 'semantic-ui-react'
 
 class SearchBar extends Component {
+
+    pesquisaTermo = e => {
+        if (e.keyCode === 13){
+            const termo = e.target.value
+            console.log(termo)
+        }
+    }
+
     render() {
         return (
             <div className='search-bar'>
                 <Segment stacked>
-                    <Input icon='search' size='large' placeholder='Search ...' />
+                    <Input icon='search' onKeyDown={(e) => this.pesquisaTermo(e)} size='large' placeholder='Search ...' />
                 </Segment>
             </div>
         )

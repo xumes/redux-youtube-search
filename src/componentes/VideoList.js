902be06df2253a6f8e1fbc028b31cbe22314a23c
Reginaldo Santos <rmsantos@br.ibm.com>
2018-10-11 10:19:23 -0300

    criando o boilerplate da aplicação criando os componentes

---
+++ src/componentes/VideoList.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoList = props => {
+    return (
+        <div>
+            <h1>Video List</h1>
+        </div>
+    )
+}
+
+export default VideoList
---
+++ src/componentes/VideoPlayer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const VideoPlayer = props => {
+    return (
+        <div>
+            <h1>Video Player</h1>
+        </div>
+    )
+}
+
+export default VideoPlayer

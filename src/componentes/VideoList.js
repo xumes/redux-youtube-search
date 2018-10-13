5df5191aac2d169302eac8e98fd2e32c55915904
Reginaldo Santos <rmsantos@br.ibm.com>
2018-10-13 18:50:45 -0300

    melhorando o componente video list

--- src/componentes/VideoList.js
+++ src/componentes/VideoList.js
@@ -1,9 +1,45 @@
 import React from 'react'
 
+import { List, Image } from 'semantic-ui-react'
+
 const VideoList = props => {
     return (
         <div className='video-list'>
-            <h1>Video List</h1>
+            <List animated verticalAlign='middle'>
+                <List.Item>
+                    <Image avatar src='' />
+                    <List.Content>
+                        <List.Header>Titulo do Video</List.Header>
+                    </List.Content>
+                </List.Item>
+            </List>
+
+            <List animated verticalAlign='middle'>
+                <List.Item>
+                    <Image avatar src='' />
+                    <List.Content>
+                        <List.Header>Titulo do Video</List.Header>
+                    </List.Content>
+                </List.Item>
+            </List>
+
+            <List animated verticalAlign='middle'>
+                <List.Item>
+                    <Image avatar src='' />
+                    <List.Content>
+                        <List.Header>Titulo do Video</List.Header>
+                    </List.Content>
+                </List.Item>
+            </List>
+
+            <List animated verticalAlign='middle'>
+                <List.Item>
+                    <Image avatar src='' />
+                    <List.Content>
+                        <List.Header>Titulo do Video</List.Header>
+                    </List.Content>
+                </List.Item>
+            </List>
         </div>
     )
 }

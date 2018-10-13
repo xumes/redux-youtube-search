2f7d1397d83bdd4e3070082226ddc16c8c5185eb
Reginaldo Santos <rmsantos@br.ibm.com>
2018-10-13 18:29:32 -0300

    conectando o projeto com o Provider

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux'
 import './App.css';
 
+import store from './store'
 import SearchBar from './componentes/SearchBar'
 import VideoList from './componentes/VideoList'
 import VideoPlayer from './componentes/VideoPlayer'
@@ -8,11 +10,13 @@ import VideoPlayer from './componentes/VideoPlayer'
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <SearchBar />
-        <VideoList />
-        <VideoPlayer />
-      </div>
+      <Provider store={store}>
+        <div className="container">
+          <SearchBar />
+          <VideoList />
+          <VideoPlayer />
+        </div>
+      </Provider>
     );
   }
 }

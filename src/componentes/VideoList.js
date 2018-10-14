0995896afc23ffc0edd841f8f603938709a93f0e
Reginaldo Santos <rmsantos@br.ibm.com>
2018-10-14 08:37:18 -0300

    diversas melhorias no componente VideoList

--- src/componentes/VideoList.js
+++ src/componentes/VideoList.js
@@ -1,47 +1,47 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {connect} from 'react-redux'
+import { List, Image, Dimmer, Loader } from 'semantic-ui-react'
 
-import { List, Image } from 'semantic-ui-react'
+class VideoList extends Component {
 
-const VideoList = props => {
-    return (
-        <div className='video-list'>
-            <List animated verticalAlign='middle'>
-                <List.Item>
-                    <Image avatar src='' />
-                    <List.Content>
-                        <List.Header>Titulo do Video</List.Header>
-                    </List.Content>
-                </List.Item>
-            </List>
+    renderVideo(video) {
+        return (
+                <List animated verticalAlign='middle' key={video.etag}>
+                    <List.Item>
+                        <Image  src={video.snippet.thumbnails.default.url} />
+                        <List.Content>
+                            <List.Header>{video.snippet.title}</List.Header>
+                        </List.Content>
+                    </List.Item>
+                </List>
+        )
+    }
 
-            <List animated verticalAlign='middle'>
-                <List.Item>
-                    <Image avatar src='' />
-                    <List.Content>
-                        <List.Header>Titulo do Video</List.Header>
-                    </List.Content>
-                </List.Item>
-            </List>
-
-            <List animated verticalAlign='middle'>
-                <List.Item>
-                    <Image avatar src='' />
-                    <List.Content>
-                        <List.Header>Titulo do Video</List.Header>
-                    </List.Content>
-                </List.Item>
-            </List>
-
-            <List animated verticalAlign='middle'>
-                <List.Item>
-                    <Image avatar src='' />
-                    <List.Content>
-                        <List.Header>Titulo do Video</List.Header>
-                    </List.Content>
-                </List.Item>
-            </List>
-        </div>
-    )
+    render(){
+        return (
+            <div className='video-list'>
+            {
+                this.props.carregando && (<Dimmer active inverted>
+                        <Loader size="medium">Carregando ...</Loader>
+                    </Dimmer>)
+            }
+                {
+                    this.props.videos.map(video => {
+                        console.log("meu video", video)
+                        return this.renderVideo(video)
+                    })
+                }
+            </div>
+        )
+    }
 }
 
-export default VideoList
+const mapStateToProps = (state) => {
+    return {
+        videos: state.busca.videos,
+        carregando: state.busca.carregando,
+        erro: state.busca.erro
+    }
+}
+
+export default connect(mapStateToProps, null)(VideoList)
